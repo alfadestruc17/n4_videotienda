a0132f31e7a6e05d6e273a4c5bb75fe64baf9d09
Samuel <rene28609@gmail.com>
2025-06-10 09:32:39 -0500

    Agrege los Atributos que tendria la copia segun su modelo conceptual

--- source/uniandes/cupi2/videotienda/mundo/Copia.java
+++ source/uniandes/cupi2/videotienda/mundo/Copia.java
@@ -1,5 +1,34 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ * $Id: Pelicula.java,v 1.1 2005/12/16 15:13:33 k-marcos Exp $
+ * Universidad de los Andes (Bogotá - Colombia)
+ * Departamento de Ingeniería de Sistemas y Computación 
+ * Licenciado bajo el esquema Academic Free License version 2.1 
+ *
+ * Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
+ * Ejercicio: n4_videotienda
+ * Autor: Katalina Marcos - Diciembre 2005
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ */
 package uniandes.cupi2.videotienda.mundo;
-
-public class Copia {
-
+/**
+ * esta clase representa una copia de una película que se encuentra en la videotienda.
+ */
+public class Copia 
+{
+	
+	// -----------------------------------------------------------------
+	// Atributos
+	// -----------------------------------------------------------------
+	
+	/**
+	 * Título de la película a la que pertenece la copia
+	 */
+     private String tituloPelicula;
+     
+     /**
+      * Código de la copia
+      */
+     private int codigo;
+     
 }
